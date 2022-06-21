8e40b47fbfd48a7a79f29109ae6df298b500132d
mukeshbaskara <57147077+mukeshbaskara@users.noreply.github.com>
2022-06-22 00:43:51 +0530

    Create hub-vnet.tf

---
+++ hub-vnet.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "vicky-poc"
+  location = "West Europe"
+}
+
+module "hub-vnet" {
+  source              = "git::https://github.com/Azure/terraform-azurerm-vnet.git"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  subnet_service_endpoints = {
+    subnet1 = ["Microsoft.Compute"]
+  }
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.rg]
+}
