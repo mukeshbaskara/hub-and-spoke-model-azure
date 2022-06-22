697a71d17f006560e1471f0041d77f98195c0904
mukeshbaskara <57147077+mukeshbaskara@users.noreply.github.com>
2022-06-23 00:38:33 +0530

    Create gateway.tf

---
+++ gateway.tf
@@ -0,0 +1,72 @@
+resource "azurerm_public_ip" "publicip" {
+  name                = "public-ip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic"
+}
+
+locals {
+  backend_address_pool_name      = "${module.hub-vnet.name}-beap"
+  frontend_port_name             = "${module.hub-vnet.name}-feport"
+  frontend_ip_configuration_name = "${module.hub-vnet.name}-feip"
+  http_setting_name              = "${module.hub-vnet.name}-be-htst"
+  listener_name                  = "${module.hub-vnet.name}-httplstn"
+  request_routing_rule_name      = "${module.hub-vnet.name}-rqrt"
+  redirect_configuration_name    = "${module.hub-vnet.name}-rdrcfg"
+}
+
+resource "azurerm_application_gateway" "gateway" {
+  name                = "front-gateway"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  sku {
+    name     = "Standard_Small"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = "${module.hub-vnet.subnet_names[0].id}"
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.publicip.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+    priority                   = 10
+  }
+}
