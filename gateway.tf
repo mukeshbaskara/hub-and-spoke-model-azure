79fcb6df1b241255e1070e545666efb12013e261
mukeshbaskara <57147077+mukeshbaskara@users.noreply.github.com>
2022-06-23 01:43:18 +0530

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "publicip" {
   name                = "public-ip"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 locals {
