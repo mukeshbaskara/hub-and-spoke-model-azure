875488ea9e77d65721e3025b6294c23628d6df8e
mukeshbaskara <57147077+mukeshbaskara@users.noreply.github.com>
2022-06-23 01:23:20 +0530

    Update gateway.tf

--- gateway.tf
+++ gateway.tf
@@ -21,7 +21,7 @@ resource "azurerm_application_gateway" "gateway" {
   location            = azurerm_resource_group.rg.location
 
   sku {
-    name     = "Standard"
+    name     = "Standard_v2"
     tier     = "Standard_v2"
     capacity = 2
   }
