957e39c78b5187f438b3a5c339bcff40b7a00dd7
mukeshbaskara <57147077+mukeshbaskara@users.noreply.github.com>
2022-06-23 01:12:49 +0530

    Update frontend-subnet.tf

--- frontend-subnet.tf
+++ frontend-subnet.tf
@@ -2,5 +2,5 @@ resource "azurerm_subnet" "frontend" {
   name                 = "frontend"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = module.hub-vnet.vnet_name
-  address_prefixes     = ["10.254.0.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
