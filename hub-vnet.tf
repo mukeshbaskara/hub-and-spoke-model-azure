cbecd5f1018aec98e168a982143be5d86cb00b28
mukeshbaskara <57147077+mukeshbaskara@users.noreply.github.com>
2022-06-23 00:48:17 +0530

    Update hub-vnet.tf

--- hub-vnet.tf
+++ hub-vnet.tf
@@ -12,8 +12,8 @@ module "hub-vnet" {
   vnet_name           = "hub-vnet"
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24"]
-  subnet_names        = ["frontend"]
+  //subnet_prefixes     = ["10.0.1.0/24"]
+  //subnet_names        = ["frontend"]
 
   //subnet_service_endpoints = {
   //  subnet1 = ["Microsoft.Storage"]
