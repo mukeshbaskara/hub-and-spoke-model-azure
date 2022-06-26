955ab9f2ecef27708c95edfb5d3411b50469fb4d
mukeshbaskara <57147077+mukeshbaskara@users.noreply.github.com>
2022-06-27 02:21:44 +0530

    Delete spoke-subnet1.tf

--- spoke-subnet1.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = module.spoke-vnet.vnet_name
-  address_prefixes     = ["10.1.1.0/24"]
-}
